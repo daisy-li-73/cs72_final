96e8203b3462d4ced61a077fcb3e444276939712
daisy-li-73 <daisy.li.26@dartmouth.edu>
2024-03-03 20:54:46 -0500

    modified scrape imsdb

--- scrape_imsdb.py
+++ scrape_imsdb.py
@@ -9,29 +9,26 @@ from selenium.webdriver.support import expected_conditions as EC
 # Parse args
 outputFilename = sys.argv[1]
 
-# Set up output file
-f = open(outputFilename, "w")
-
 # Initialize Chrome driver
 options = ChromeOptions()
 options.add_argument("--headless=new")
 driver = webdriver.Chrome(options=options)
 driver.get("https://imsdb.com/all-scripts.html")
 
-# Get all movie scripts
-movies = driver.find_elements(By.CSS_SELECTOR, "p")
+with open(outputFilename, "w", encoding="utf-8") as f:
+  # Get all movie scripts
+  movies = driver.find_elements(By.CSS_SELECTOR, "p")
 
-# Iterate over each movie to extract its title, writers, script link and write into CSV
-for m in movies:
-  a_tag = m.find_element(By.TAG_NAME, "a")
-  title = a_tag.text
+  # Iterate over each movie to extract its title, writers, script link and write into CSV
+  for m in movies:
+    a_tag = m.find_element(By.TAG_NAME, "a")
+    title = a_tag.text
 
-  # Get writers
-  i_tag = m.find_element(By.TAG_NAME, "i")
-  writers = i_tag.text  # This will include 'Written by'
-  writers = writers.replace("Written by ", "") 
+    # Get writers
+    i_tag = m.find_element(By.TAG_NAME, "i")
+    writers = i_tag.text  # This will include 'Written by'
+    writers = writers.replace("Written by ", "") 
 
-  f.write(title+","+writers+"\n")
+    f.write(title+","+writers+"\n")
    
-f.close()
 driver.quit()    
