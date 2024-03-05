b4b4920b742cc0418dcde3b53d263963af7ce2b7
daisy-li-73 <daisy.li.26@dartmouth.edu>
2024-03-05 06:56:04 -0500

    added script donwloads

--- scrape_rt.py
+++ scrape_rt.py
@@ -5,100 +5,133 @@ from selenium.webdriver.common.keys import Keys
 import sys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException  
 
-# Parse args
-inputFilename = sys.argv[1]
-outputFilename = sys.argv[2]
+def names_match(name1, name2):
+  # Split both names into components based on spaces
+  name1_parts = name1.split()
+  name2_parts = name2.split()
+  
+  # Count the number of matching components
+  match_count = sum(1 for part in name1_parts if part in name2_parts)
+  
+  # Consider it a match if there are at least 2 matching components
+  return match_count >= 2
 
-# Set up output file
-f = open(outputFilename, "w")
-f.write("IMSDB_Title,RT_Title,CriticScore,AudienceScore\n")
+def main():
+  # Parse args
+  inputFilename = sys.argv[1]
+  outputFilename = sys.argv[2]
 
-# Initialize Chrome driver
-options = ChromeOptions()
-options.add_argument("--headless=new")
-# driver = webdriver.Chrome(options=options)
-driver = webdriver.Chrome()
-driver.get("https://www.rottentomatoes.com/")
+  # Set up output file
+  with open(outputFilename, "w") as f:
+    f.write("IMSDB_Title,RT_Title,CriticScore,AudienceScore\n")
 
-# Read input file
-with open(inputFilename, "r") as file:
-  for line in file:
-    found = False
+    # Initialize Chrome driver
+    options = ChromeOptions()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
+    # driver = webdriver.Chrome()
+    driver.get("https://www.rottentomatoes.com/")
 
-    # Parse input
-    parsed_input = line.strip()
-    parsed_input = parsed_input.split(",")
-    imsdb_title = parsed_input[0]
-    imsdb_writers = parsed_input[1:]
-    imsdb_writers_set = set(imsdb_writers)
+    # Read input file
+    with open(inputFilename, "r") as file:
+      driver.get("https://www.rottentomatoes.com/")
+      for line in file:
+        found = False
 
-    # Write IMSDB title to output file
-    f.write(imsdb_title+",")
+        # Parse input
+        parsed_input = line.strip()
+        parsed_input = parsed_input.split(",")
+        imsdb_title = parsed_input[0]
+        imsdb_writers = parsed_input[1:]
+        imsdb_writers_set = set(imsdb_writers)
 
-    # Search for movie
-    search_string = imsdb_title
-    for writer in imsdb_writers:
-      search_string += " "+writer
-    search_bar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "search-text")))
-    search_bar.click()
-    search_bar.send_keys(search_string)
-    search_submit = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "search-submit")))
-    search_submit.click()
+        # Write IMSDB title to output file
+        f.write(imsdb_title+",")
 
-    # Locate the search results component
-    search_results = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "ul[slot='list']"))
-    )
-    
-    result_items = search_results.find_elements(By.CSS_SELECTOR, "search-page-media-row")  # Adjust the selector as necessary
-    result_urls = [result.find_element(By.CSS_SELECTOR, "a[data-qa='info-name']").get_attribute("href") for result in result_items]
-    # Iterate through the results
-    for result in result_urls:
-      driver.get(result)
-      # Get list of writers from the movie's page
-      movie_info = driver.find_element(By.ID, "info")
-      li_elements = movie_info.find_elements(By.TAG_NAME, "li")
-      if len(li_elements) < 6: # Defensive programming
-        f.write(",,,")
-        continue
-      writers_element = li_elements[5]
-      span_element = writers_element.find_element(By.CSS_SELECTOR, "span[data-qa='movie-info-item-value']")
-      writer_links = span_element.find_elements(By.TAG_NAME, "a")
-      rt_writers = [link.text for link in writer_links]
-
-      # Check if the writers match      
-      rt_writers_set = set(rt_writers)
-      common_writers = imsdb_writers_set.intersection(rt_writers_set)
-      # threshold = min(len(imsdb_writers_set), len(rt_writers_set)) / 2
-      # If match found, get audience and critic scores and write to output file
-      if common_writers == imsdb_writers_set:
-        rt_title = driver.find_element(By.CLASS_NAME, "title").text
-
+        # Search for movie
         try:
-          scoreboard = WebDriverWait(driver, 10).until(
-              EC.presence_of_element_located((By.ID, "scoreboard"))
-          )
-          critic_score = scoreboard.get_attribute("tomatometerscore")
-          audience_score = scoreboard.get_attribute("audiencescore")
-
+          search_bar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "search-text")))
         except TimeoutException:
-          f.write(",,,")
+          f.write("ERROR\n")
           continue
-        except NoSuchElementException:
-          f.write(",,,")
-          continue
+        search_bar.click()
+        print(imsdb_title)
+        search_bar.send_keys(imsdb_title)
+        search_bar.send_keys(Keys.RETURN)
+        # search_submit = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "search-submit")))
+        # search_submit.click()
 
-        f.write(f"{rt_title},{critic_score},{audience_score}\n")
-        found = True
-        break
-    
-    if found == False:
-      f.write(",,,")
+        # Locate the search results component
+        try:
+          search_results = WebDriverWait(driver, 10).until(
+          EC.presence_of_element_located((By.CSS_SELECTOR, "search-page-result[type='movie']"))
+          )
+        except TimeoutException:
+          f.write("NOTFOUND\n") 
+          continue 
+        
+        result_items = search_results.find_elements(By.CSS_SELECTOR, "search-page-media-row")  # Adjust the selector as necessary
+        result_urls = [result.find_element(By.CSS_SELECTOR, "a[data-qa='info-name']").get_attribute("href") for result in result_items]
+        # Iterate through the results
+        for result in result_urls:
+          driver.get(result)
+          # Get list of writers from the movie's page
+          movie_info = driver.find_element(By.ID, "info")
+          li_elements = movie_info.find_elements(By.TAG_NAME, "li")
+          if len(li_elements) < 6: # Defensive programming
+            continue
+          writers_element = li_elements[5]
+          span_element = writers_element.find_element(By.CSS_SELECTOR, "span[data-qa='movie-info-item-value']")
+          writer_links = span_element.find_elements(By.TAG_NAME, "a")
+          rt_writers = [link.text for link in writer_links]
 
-f.close()
-driver.quit()
+          # Check if the writers match      
+          rt_writers_set = set(rt_writers)
+          common_writers = imsdb_writers_set.intersection(rt_writers_set)
 
-    
+          # Prepare sets to hold writers not found in the initial intersection
+          unmatched_imsdb_writers = imsdb_writers_set - common_writers
+          unmatched_rt_writers = rt_writers_set - common_writers
+
+          if len(unmatched_imsdb_writers) != 0 or len(unmatched_rt_writers) != 0:
+            for writer1 in unmatched_imsdb_writers:
+              # Check against each writer in the second set
+              for writer2 in unmatched_rt_writers:
+                  # If a match is found based on our custom logic, add to the common writers set
+                  if names_match(writer1, writer2):
+                      common_writers.add(writer1)
+                      unmatched_rt_writers.discard(writer2)
+                      break  # Move on to the next writer after finding a match
+
+          unmatched_imsdb_writers = unmatched_imsdb_writers - common_writers
+          # If match found, get audience and critic scores and write to output file
+          if len(unmatched_imsdb_writers) == 0 or len(unmatched_rt_writers) == 0:
+            rt_title = driver.find_element(By.CLASS_NAME, "title").text
+
+            try:
+              scoreboard = WebDriverWait(driver, 10).until(
+                  EC.presence_of_element_located((By.ID, "scoreboard"))
+              )
+              critic_score = scoreboard.get_attribute("tomatometerscore")
+              audience_score = scoreboard.get_attribute("audiencescore")
+
+            except TimeoutException:
+              critic_score = "NOTFOUND"
+              audience_score = "NOTFOUND"
+              continue
+
+            f.write(f"{rt_title},{critic_score},{audience_score}\n")
+            found = True
+            break
+        
+        if found == False:
+          f.write("NOTFOUND\n")
+
+  driver.quit()
+
+if __name__ == "__main__":
+    main()   
 
 
